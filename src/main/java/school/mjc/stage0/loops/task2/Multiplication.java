7260f91ef3ed35486df00d2717af9421800cd114
NeverRIEght <lfkomarovme@gmail.com>
2023-07-07 14:42:51 +0300

    Multiplication

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        boolean isNegative = multiplyByAndToInclusive < 0 ? true : false;
+        int counter = 0;
+
+        if(!isNegative) {
+            while (counter <= multiplyByAndToInclusive) {
+                System.out.println(counter * multiplyByAndToInclusive);
+                counter++;
+            }
+        } else {
+            while (counter <= multiplyByAndToInclusive * (-1)) {
+                System.out.println(counter * multiplyByAndToInclusive * (-1));
+                counter++;
+            }
+        }
     }
 }
