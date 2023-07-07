d6b6e5076fb505fe7470d2fe9916e994b80a3c02
NeverRIEght <lfkomarovme@gmail.com>
2023-07-07 10:02:33 +0300

    Natural numbers

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+        int counter = 0;
+        while(counter <= 9) {
+            System.out.println(counter);
+        }
     }
 }
