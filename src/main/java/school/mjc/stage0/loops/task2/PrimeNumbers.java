0494e4a32d5dd761f631f4f78d7a12a02c40ba5b
NeverRIEght <lfkomarovme@gmail.com>
2023-07-07 23:09:35 +0300

    Simple numbers

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 0;
+        while(counter <= printToInclusive) {
+            if(counter >= 0 && counter <= 9) {
+                System.out.println(counter);
+            }
+        }
     }
 }
