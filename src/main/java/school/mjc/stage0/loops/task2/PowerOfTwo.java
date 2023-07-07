4b26335813de39f3f9a0f25bfd965c7b37457abe
NeverRIEght <lfkomarovme@gmail.com>
2023-07-07 23:30:50 +0300

    Power of two

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
+        boolean isNegative = power < 0;
+        int counter = 0;
 
+        if(isNegative) {
+            System.out.println("too much power");
+        } else {
+            while(counter <= power) {
+                System.out.println(2^counter);
+                counter++;
+            }
+        }
     }
 }
