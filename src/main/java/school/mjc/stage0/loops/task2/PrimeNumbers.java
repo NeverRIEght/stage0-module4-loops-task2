af57e1decea6b5c6a158a88dfd29ffadd94e55d5
NeverRIEght <lfkomarovme@gmail.com>
2023-07-08 10:59:55 +0300

    Fix Simple numbers Now output contains prime numbers only

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -3,12 +3,22 @@ package school.mjc.stage0.loops.task2;
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
         if(printToInclusive != 0) {
-            int counter = 0;
-            while(counter <= printToInclusive) {
-                if(counter >= 0 && counter <= 9) {
-                    System.out.println(counter);
+            int i = 0;
+            while(i <= printToInclusive) {
+                if(i >= 2) {
+                    int j = 2;
+                    boolean isPrime = true;
+                    while(j <= i / 2) {
+                        if(i % j == 0) {
+                            isPrime = false;
+                        }
+                        j++;
+                    }
+                    if(isPrime == true) {
+                        System.out.println(i);
+                    }
                 }
-                counter++;
+                i++;
             }
         }
     }
